f0dc1975d05078926d9f21bf5bb4f8b4ff9b407e
Shamil GHASEETA <shamil.ghaseeta@gmail.com>
2026-04-21 18:10:24 +0200

    test: fix Routes unit tests

--- tests/app/Routes.test.tsx
+++ tests/app/Routes.test.tsx
@@ -3,17 +3,34 @@ import ShopPage from '@/app/shop/page'
 import WorkshopsPage from '@/app/workshops/page'
 import CommissionsPage from '@/app/commissions/page'
 
+// Mock matchMedia
+beforeAll(() => {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // Deprecated
+      removeListener: jest.fn(), // Deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  })
+})
+
 describe('Basic Routes', () => {
   it('renders Shop page', () => {
     render(<ShopPage />)
-    expect(screen.getByText('The Shop')).toBeInTheDocument()
+    expect(screen.getByText(/La Boutique est actuellement/i)).toBeInTheDocument()
   })
   it('renders Workshops page', () => {
     render(<WorkshopsPage />)
-    expect(screen.getByText('Workshops')).toBeInTheDocument()
+    expect(screen.getByText(/Transmettre le/i)).toBeInTheDocument()
   })
   it('renders Commissions page', () => {
     render(<CommissionsPage />)
-    expect(screen.getByText('Custom Commissions')).toBeInTheDocument()
+    expect(screen.getByText(/Des pièces conçues/i)).toBeInTheDocument()
   })
 })
