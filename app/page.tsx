0633b3618b2f2a0d59dd38e60b22adbc1a854f75
Shamil GHASEETA <shamil.ghaseeta@gmail.com>
2026-04-24 21:10:22 +0200

    Add ISR revalidate for dynamic content

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { getHomePageContent } from '@/lib/content'
 import HomeClient from './home-page'
 
+export const revalidate = 60
+
 export default function Home() {
   const content = getHomePageContent()
   return <HomeClient content={content} />
